95b442f7e826ea3e6d4af641e2a36dfd35e9ef1b
Mark Roberts <markroberts.jnr@virginmedia.com>
2018-11-24 19:16:21 +0000

    find error in code

--- posts/views.py
+++ posts/views.py
@@ -12,12 +12,12 @@ def get_posts(request):
     
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
-    
+
 def post_detail(request, pk):
     """
     Create a view that returns a single
     Post object based on the post ID (pk) and
-    render it to the 'postdetail.html' template.and
+    render it to the 'postdetail.html' template.
     Or return a 404 error if the post is
     not found
     """
@@ -25,7 +25,7 @@ def post_detail(request, pk):
     post.views += 1
     post.save()
     return render(request, "postdetail.html", {'post': post})
-    
+
 def create_or_edit_post(request, pk=None):
     """
     Create a view that allows us to create
@@ -40,5 +40,5 @@ def create_or_edit_post(request, pk=None):
             return redirect(post_detail, post.pk)
     else:
         form = BlogPostForm(instance=post)
-        return render(request, 'blogpostform.html', {'form': form})
-        
+    return render(request, 'blogpostform.html', {'form': form})
+    
